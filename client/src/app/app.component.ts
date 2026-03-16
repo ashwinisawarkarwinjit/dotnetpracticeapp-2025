c78ab0af4d568443a57c4fd5d9d005baeb876ab1
AshwiniS <ashwini.sawarkar@rib-software.com>
2026-03-16 11:54:27 +0530

    end of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  private readonly http= inject(HttpClient);
+  protected title = 'client !!!';
+  protected members = signal<any>([]);
+  
+  // ngOnInit(): void {
+  //   this.http.get('https://localhost:5001/api/members').subscribe({
+  //     next: response => this.members.set(response),
+  //     error: error => console.log(error),
+  //     complete:()=> console.log('completed the http request')
+  //   })
+  // }
+
+  public async ngOnInit() {
+    this.members.set(await this.getMembers());
+  }
+  
+  public async getMembers(){
+    try{
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+
+    }catch(error){
+      console.log(error);
+      throw error;
+    }
+  }
+}
